021361decc16eeb7c797a980e84c543538a19a88
fumihiko kakuma <kakuma@valinux.co.jp>
2016-11-29 14:52:10 +0900

    Do not use postgresql
    Signed-off-by: Fumihiko Kakuma <kakuma@valinux.co.jp>

--- ryu/tests/integrated/openstack/fullstack_main.sh
+++ ryu/tests/integrated/openstack/fullstack_main.sh
@@ -12,10 +12,17 @@ BASEDIR=$BASE/new
 sudo mkdir -p $BASEDIR
 sudo chown -R $USER:root $BASE
 
+sudo apt-get update
+sudo apt-get install -qy patch
+
 pushd $BASEDIR
 sudo pip install -U tox
 git clone https://git.openstack.org/openstack-dev/devstack
 git clone https://git.openstack.org/openstack/neutron
+
+pushd neutron
+patch -p1 < $SCRIPT_PATH/no-pg.patch
+popd
 
 sudo $BASEDIR/devstack/tools/create-stack-user.sh
 #sudo chown -R $STACK_USER:$STACK_USER /opt/stack
